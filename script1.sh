d9ee5d4489ca6cfc6c7b2bf16e8294a0b0097eb8
Manu2806 <manasvenikilnagar@gmail.com>
2017-02-09 22:44:41 +0000

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Enter proteincode and press [ENTER]:"
+echo "Enter (userinput) and press [ENTER]:"
 #to obtain user input
 
-read proteincode
+read userinput
 
-$proteincode= proteincode
+$userinput= userinput
 #to assign a variable to user input
 
-curl --remote-name https://files.rcsb.org/view/$proteincode.pdb
+curl --remote-name https://files.rcsb.org/view/$userinput.pdb
 #curl function with remote name for doenloading any pdb code file (defined by variable) from the website
 
-grep ATOM $proteincode.pdb | grep -v REMARK | cut -c 18-21,24-26 | sort | uniq | cut -f 1 -d " " | uniq -c | sort -nr
+grep ATOM $userinput.pdb | grep -v REMARK | cut -c (element position) | sort | uniq | cut -f 1 -d " " | uniq -c | sort -nr
 #to display amino acid occurance in the protein on the interface
 
 
